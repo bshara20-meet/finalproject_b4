08063c447a19d8cdfb21bd96708fbdbd3350db8b
myname21-meet <myname21@meet.mit.edu>
2019-08-06 10:34:57 -0400

    last coupon page

--- app.py
+++ app.py
@@ -8,5 +8,9 @@ app = Flask(__name__)
 def index():
 	return render_template('coupons.html')
 
+@app.route("/coupons/")
+def coupons():
+	return render_template('coupons.html')
+
 if __name__ == '__main__':
 	app.run(debug=True)
