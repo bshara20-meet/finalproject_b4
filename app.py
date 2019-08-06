8b373a081f4e7e5790b20f55d8f899b697183841
myname21-meet <myname21@meet.mit.edu>
2019-08-06 11:33:11 -0400

    last version

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ def index():
 @app.route("/coupons/")
 def coupons():
 	return render_template('coupons.html')
+	
+@app.route('/join-us/')
+def join1():
+	return render_template('join1.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
